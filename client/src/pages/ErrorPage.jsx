4882842843c056a27faa1cf9e31ff59eaafb9011
nghia <nghianm2803@gmail.com>
2023-09-19 11:05:21 +0700

    update not found page

--- client/src/pages/ErrorPage.jsx
+++ client/src/pages/ErrorPage.jsx
@@ -1,5 +1,6 @@
 import { Link as RouterLink, useRouteError } from "react-router-dom";
 import { Box, Button, Typography, Container } from "@mui/material";
+import NotFoundImage from "../assets/404.svg";
 
 function ErrorPage() {
   const error = useRouteError();
@@ -13,11 +14,17 @@ function ErrorPage() {
       }}
     >
       <Box sx={{ maxWidth: 480, margin: "auto", textAlign: "center" }}>
+        <Box
+          component="img"
+          alt="Have an Error"
+          src={NotFoundImage}
+          sx={{ width: "300px" }}
+        />
         <Typography variant="h4" paragraph>
-          Page not found!
+          404 ERROR
         </Typography>
         <Typography sx={{ color: "text.secondary", mb: "1rem" }}>
-          Sorry, we couldn't find the page you requested.
+          Opps. Have an error, please try again or go Home.
         </Typography>
         <Typography sx={{ color: "text.secondary", mb: "1rem" }}>
           {error.statusText || error.message}
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
-    global: 'window',
+    global: "window",
   },
   build: {
     rollupOptions: {
-      external: ['graphql'],
+      external: ["graphql"],
     },
   },
 });
